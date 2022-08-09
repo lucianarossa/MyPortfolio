19995a6fdb01897f8ef9b59448b08a08569f8b58
luciana rossa <lucianarossa@gmail.com>
2022-08-09 12:22:45 -0300

    inicio

---
+++ src/components/Cover.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Cover(){
+    return(
+        <div>Hola!</div>
+    )
+}
+
+export default Cover
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Cover from "../components/Cover.jsx"
+
+function Home() {
+    return (
+     <Cover/>
+    );
+  }
+  
+  export default Home;
+  
